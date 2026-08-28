03e9887730d646fe5dc8068a371bdaed32e67306
nors <nors@inception-of-things.myguest.virtualbox.org>
2026-08-28 23:05:58 +0100

    p2

--- p2/scripts/setup-server.sh
+++ p2/scripts/setup-server.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 sudo apt-get update -y
-sudo apt-get install -y curl iptables
+sudo apt-get install -y curl net-tools
+
+# Make ifconfig reachable for all users
+sudo ln -sf /usr/sbin/ifconfig /usr/bin/ifconfig
 
 export INSTALL_K3S_BIN_DIR="/usr/bin"
 export INSTALL_K3S_EXEC="server --node-ip=192.168.56.110 --write-kubeconfig-mode=644 --disable metrics-server --disable local-storage"
 curl -sfL https://get.k3s.io | sh -
 
-until kubectl wait --for=condition=Ready node --all --timeout=10s 2>/dev/null; do sleep 2; done
+kubectl wait --for=condition=Ready node --all --timeout=60s
 
 kubectl apply -f /home/vagrant/confs/manifest.yaml
